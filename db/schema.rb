3ec1e3dfb127c929eab59598ea999f6d0b7b6520
kumargopalraj <gmdctbe@yahoo.com>
2018-10-28 18:04:15 +0530

    ui related files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_112400) do
+ActiveRecord::Schema.define(version: 2018_10_28_120726) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "tagline"
+    t.string "image"
+    t.integer "user_id"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
