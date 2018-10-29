c49df2753457cfdaf966c0ea0a92cc90af2582c3
kumargopalraj <gmdctbe@yahoo.com>
2018-10-29 13:25:04 +0530

    final bug fixes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,15 +32,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+  include CarrierWave::MiniMagick
+  require 'carrierwave/processing/mini_magick'
   version :thumb do
      process :resize_to_fit => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
